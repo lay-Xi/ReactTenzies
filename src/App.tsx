195d276c2670e0851535166db8baa1d94ef9e404
Xi Gao <xiga6451@colorado.edu>
2022-04-26 17:20:52 -0600

    Added reroll button

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,34 @@
+import { useState } from 'react';
 import Die from './components/Die';
 
 export default function App() {
-
-  const allNewDice: Function = () => {
+  const allNewDice = () => {
     let randomNumbers = [];
 
     for (let i = 0; i < 10; i++) {
-      randomNumbers.push(Math.floor((Math.random() * 5) + 1));
+      randomNumbers.push(Math.floor(Math.random() * 5 + 1));
     }
-    
+
     return randomNumbers;
+  };
+
+  const [dices, setDices] = useState(allNewDice());
+
+  const rollDice = () => {
+    setDices(allNewDice());
   }
+
+  let diceCount = 0;
 
   return (
     <main>
       <div className='dice--container'>
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
-        <Die value={5} />
-        <Die value={6} />
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
+        {dices.map((diceValue: number) => {
+          diceCount++;
+          return <Die key={diceCount} value={diceValue} />;
+        })}
       </div>
+      <button className='roll-dice' onClick={rollDice}>Roll</button>
     </main>
   );
 }
